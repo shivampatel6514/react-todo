00e61a453f64a2581536593a07becaa7ba121993
shivampatel6514 <shivampatel6514@gmail.com>
2022-07-28 16:22:34 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import ToDoLists from './ToDoLists';
 function App() {
+
+  const [inpulList, setInpulList] = useState('');
+  const [items, setItems] = useState([]);
+
+
+  const itemEvent = (event) => {
+    setInpulList(event.target.value)
+  };
+  const listSave = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inpulList];
+    })
+    setInpulList("");
+
+  };
+  const deleteItems = (id) =>{
+    console.log(id);
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem ,index) => {
+        return index !== id;
+      })
+    })
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input type="text" placeholder="Add Todo's" value={inpulList} onChange={itemEvent} />
+          <button onClick={listSave} > + </button>
+
+          <ol>
+
+            {
+              items.map((itemvalue, i) => {
+                return(  
+                  <ToDoLists 
+                    key={i} 
+                    id={i} 
+                    text={itemvalue}
+                    onSelect={deleteItems}
+                  />);
+              })
+            }
+                {/* return <li key={i}>{itemvalue}</li> */}
+          </ol>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/ToDoLists.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ToDoLists = (props) =>{
+    
+    return( 
+        <>
+            <div className="todo_style">
+                <i 
+                    className="fa fa-times" 
+                    onClick={() =>{
+                        props.onSelect(props.id);
+                    }} 
+                    aria-hidden="true" />
+                <li >{props.text}</li>
+            </div>
+        </>
+    )
+}
+
+export default ToDoLists;
